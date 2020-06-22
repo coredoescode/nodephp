84173dfa9a76b46bf54208959718e6b610c23aea
coredoescode <36366790+coredoescode@users.noreply.github.com>
2020-06-22 18:10:05 -0400

    Update and rename nodephp-install.sh to nodephp-install-dl.sh

--- nodephp-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-nvm install node
-nvm alias default node
-echo -e "export PATH=\"$PATH:$HOME\/local\/bin\" >> ~/.bashrc
